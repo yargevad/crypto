277a74dcf9c2d666790a260684b95ce2c0fe9171
Dave Gray <yargevad+github@gmail.com>
2016-10-17 23:03:13 -0600

    remove unnecessary alias; switch to context-tracking errors package

--- naclutil/naclutil.go
+++ naclutil/naclutil.go
@@ -7,7 +7,8 @@ import (
 	"io/ioutil"
 	"os"
 
-	box "golang.org/x/crypto/nacl/box"
+	"github.com/pkg/errors"
+	"golang.org/x/crypto/nacl/box"
 )
 
 const (
@@ -20,22 +21,22 @@ func CreateKeyStore(path string) error {
 	fi, err := os.Stat(path)
 	if err != nil {
 		if !os.IsNotExist(err) {
-			return err
+			return errors.Wrap(err, "stat failed")
 		}
 		err = os.MkdirAll(path, 0700)
 		if err != nil {
-			return err
+			return errors.Wrap(err, "mkdir failed")
 		}
 		fi, err = os.Stat(path)
 		if err != nil {
-			return err
+			return errors.Wrap(err, "stat2 failed")
 		}
 	}
 	if fi == nil {
-		return err
+		return errors.Wrap(err, "stat returned nil")
 	}
 	if !fi.IsDir() {
-		return err
+		return errors.Wrap(err, "CreateKeyStore takes a directory")
 	}
 	return nil
 }
@@ -52,7 +53,7 @@ func FetchKeypair(path, name string) ([]byte, []byte, error) {
 	pubExists := true
 	if err != nil {
 		if !os.IsNotExist(err) {
-			return nil, nil, err
+			return nil, nil, errors.Wrap(err, "stat failed")
 		}
 		pubExists = false
 	}
@@ -62,41 +63,41 @@ func FetchKeypair(path, name string) ([]byte, []byte, error) {
 	keyExists := true
 	if err != nil {
 		if !os.IsNotExist(err) {
-			return nil, nil, err
+			return nil, nil, errors.Wrap(err, "stat failed")
 		}
 		keyExists = false
 	}
 
 	/* Unacceptable: one path exists and other does not */
 	if pubExists != keyExists {
-		err = fmt.Errorf("need neither or both of %s %s\n", pubPath, keyPath)
+		err = errors.Errorf("need neither or both of %s %s\n", pubPath, keyPath)
 		return nil, nil, err
 	}
 
 	/* Unacceptable: either path is a directory */
 	if pubExists && pubInfo.IsDir() {
-		err = fmt.Errorf("found directory at %s\n", pubPath)
+		err = errors.Errorf("found directory at %s\n", pubPath)
 		return nil, nil, err
 	}
 	if keyExists && keyInfo.IsDir() {
-		err = fmt.Errorf("found directory at %s\n", keyPath)
+		err = errors.Errorf("found directory at %s\n", keyPath)
 		return nil, nil, err
 	}
 
 	if !pubExists {
 		pub, key, err := box.GenerateKey(rand.Reader)
 		if err != nil {
-			return nil, nil, err
+			return nil, nil, errors.Wrap(err, "key generation failed")
 		}
 
 		err = ioutil.WriteFile(pubPath, (*pub)[:], 0644)
 		if err != nil {
-			return nil, nil, err
+			return nil, nil, errors.Wrap(err, "public key write failed")
 		}
 
 		err = ioutil.WriteFile(keyPath, (*key)[:], 0600)
 		if err != nil {
-			return nil, nil, err
+			return nil, nil, errors.Wrap(err, "private key write failed")
 		}
 
 		return (*pub)[:], (*key)[:], nil
@@ -104,11 +105,11 @@ func FetchKeypair(path, name string) ([]byte, []byte, error) {
 	} else {
 		pub, err := ioutil.ReadFile(pubPath)
 		if err != nil {
-			return nil, nil, err
+			return nil, nil, errors.Wrap(err, "public key read failed")
 		}
 		key, err := ioutil.ReadFile(keyPath)
 		if err != nil {
-			return nil, nil, err
+			return nil, nil, errors.Wrap(err, "private key read failed")
 		}
 
 		return pub, key, nil
@@ -119,7 +120,7 @@ func EncryptMessage(msg, _toPub, _fromKey []byte) ([]byte, []byte, error) {
 	var nonce [NonceLength]byte
 	_, err := rand.Read(nonce[:])
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, errors.Wrap(err, "nonce read failed")
 	}
 	/* Copy byte slices into the static arrays that box.Seal expects. */
 	var toPub, fromKey [KeyLength]byte
@@ -137,7 +138,7 @@ func DecryptMessage(enc, _nonce, _fromPub, _toKey []byte) ([]byte, error) {
 	copy(toKey[:], _toKey)
 	msg, success := box.Open(nil, enc, &nonce, &fromPub, &toKey)
 	if !success {
-		return nil, fmt.Errorf("failed to decrypt message")
+		return nil, errors.Errorf("failed to decrypt message")
 	}
 	return msg, nil
 }
